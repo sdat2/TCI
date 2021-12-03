bf21ac7fc66cc16386f7547bdb6113d825b23c18
Simon Thomas <sdat2@cam.ac.uk>
2021-12-03 16:56:02 +0000

    ext: moving right files to correct branch

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
-from setuptools import setup, find_packages
+"""setup.py allows the installation of the project by pip."""
+from setuptools import find_packages, setup
 
 setup(
     name="tci",
-    version="1.0.0",
-    url="https://github.com/wy2136/TCI.git",
-    author="Wenchang Yang",
-    author_email="wenchang@princeton.edu",
-    description="Calculate TC indices",
+    version="0.0.1",
+    #author="Simon Thomas",
+    #author_email="sdat2@cam.ac.uk",
+    #description="ENSO MRes project 2020",
+    #url="https://github.com/sdat2/seager19",
     packages=find_packages(),
-    install_requires=["numpy >= 1.11.1", "matplotlib >= 1.5.1", "tcpypi"],
+    # test_suite="src.tests.test_all.suite",
+    # setup_requires=["pytest-runner"],
+    # tests_require=["pytest"],
 )
