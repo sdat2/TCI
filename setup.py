efbf14a36df421d51fa7ad443a539701fad930a7
Simon Thomas <sdat2@cam.ac.uk>
2021-12-03 13:17:28 +0000

    ext: adding notebooks and setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+"""setup.py allows the installation of the project by pip."""
+from setuptools import find_packages, setup
+
+setup(
+    name="tci",
+    version="0.0.1",
+    #author="Simon Thomas",
+    #author_email="sdat2@cam.ac.uk",
+    #description="ENSO MRes project 2020",
+    #url="https://github.com/sdat2/seager19",
+    packages=find_packages(),
+    # test_suite="src.tests.test_all.suite",
+    # setup_requires=["pytest-runner"],
+    # tests_require=["pytest"],
+)
